ad77f5279734bdb8e315c7d4c5eabe015ae2248d
guozhifu <guozf@163.com>
2019-11-20 17:30:39 +0800

    不同环境打包

---
+++ config/test.env.js
@@ -0,0 +1,8 @@
+'use strict'
+module.exports = {
+  NODE_ENV: '"development"',
+  API_ROOT: '"//172.24.182.76:8081"',
+  GOOGLE_APP_CLIENT_ID:'"157122725629-vmjji99ukd62pbdmhl0ul1cnir7hip89.apps.googleusercontent.com"',
+  FACEBOOK_APP_CLIENT_ID:'"685267178601621"',
+  COOKIE_DOMAIN:'".bwplus.com.tw"',
+}
