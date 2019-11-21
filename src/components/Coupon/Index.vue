eda412d82355dab643a91c741955875965cc7d11
sean <sean_su@bwnet.com.tw>
2019-11-21 16:06:45 +0800

    [Update]樣式修正完成

--- src/components/Coupon/Index.vue
+++ src/components/Coupon/Index.vue
@@ -1,9 +1,8 @@
 <template>
   <section id="coupon">
-    <link rel="stylesheet" href="https://172.24.222.222:5757/style.css">
     
     <!-- coupon page title -->
-    <div class="coupon_page_title">查看我的兌換券</div>
+    <div class="coupon_page_title"></div>
 
     <!-- coupon list -->
     <div class="coupon_list_wrap">
@@ -15,7 +14,7 @@
 
           <!-- coupon image -->
           <div class="cover">
-            <div class="img" style="background-image: url(https://fakeimg.pl/245x365/)"></div>
+            <div class="img" style="background-image: url(https://fakeimg.pl/600x600/)"></div>
           </div>
 
           <!-- coupon name -->
@@ -29,7 +28,7 @@
               <strong>320</strong>
             </div>
             <div>
-              <small>商周點</small>
+              <small>商周幣</small>
               <span>1656</span>
             </div>
 
@@ -42,64 +41,89 @@
     </div>
 
     <!-- coupon detail -->
-    <div class="coupon_deatil text-center" :class="{ 'active': couponDeatilOpen, 'overlay': confirm }">
+    <div class="coupon_detail text-center" :class="{ 'active': couponDeatilOpen, 'overlay': confirm }">
 
-      <!-- coupon name -->
-      <div class="name">30秒專注力法則</div>
+      <div class="coupon_detail_contents">
 
-      <img class="d-block" src="https://fakeimg.pl/245x365/" alt="">
-
-      <!-- B coin -->
-      <div class="b-coin">
-        <strong>320</strong>
-      </div>
-
-      <div class="info">
-        可用商周點 / 1234
-      </div>
-
-      <!-- collapse -->
-      <div class="collapse-group">
-        
-        <div class="collapse">
-          <!-- title-->
-          <div class="title-box">
-            <div class="title">兌換時間</div>
-            <div class="icons"><span class="d-block"></span><span class="d-block"></span></div>
-          </div>
-          <!-- contents-->
-          <div class="contents-box">
-            <div class="contents">
-              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquam ducimus, illum vitae nam officia quidem iste maxime provident! Temporibus repellat nulla blanditiis at accusamus minima delectus aut doloribus officia deserunt.</p>
-            </div>
-          </div>
+        <!-- coupon top -->
+        <div class="top">
+          <button type="button" class="close" @click="closeConfirm">&times;</button>
         </div>
 
-        <div class="collapse">
-          <div class="title-box">
-            <div class="title">獎項使用說明</div>
-            <div class="icons"><span></span><span></span></div>
-          </div>
-          <div class="contents-box">
-            <div class="contents">
-              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquam ducimus, illum vitae nam officia quidem iste maxime provident! Temporibus repellat nulla blanditiis at accusamus minima delectus aut doloribus officia deserunt.</p>
-            </div>
-          </div>
+        <!-- coupon name -->
+        <div class="name">30秒專注力法則</div>
+
+        <figure>
+          <img class="d-block" src="https://fakeimg.pl/600x600/" alt="">
+        </figure>
+
+        <!-- B coin -->
+        <div class="b-coin">
+          <strong>320</strong>
         </div>
 
+        <div class="info">可用商周幣 / 1234</div>
+
+        <!-- collapse -->
+        <div class="collapse-group">
+          
+          <div class="collapse">
+            <!-- title-->
+            <div class="title-box waves">
+              <div class="title">兌換時間</div>
+              <div class="icons"><span class="d-block"></span><span class="d-block"></span></div>
+            </div>
+            <!-- contents-->
+            <div class="contents-box">
+              <div class="contents">
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquam ducimus, illum vitae nam officia quidem iste maxime provident! Temporibus repellat nulla blanditiis at accusamus minima delectus aut doloribus officia deserunt.</p>
+              </div>
+            </div>
+          </div>
+
+          <div class="collapse">
+            <div class="title-box waves">
+              <div class="title">獎項使用說明</div>
+              <div class="icons"><span class="d-block"></span><span class="d-block"></span></div>
+            </div>
+            <div class="contents-box">
+              <div class="contents">
+                <ul>
+                  <li>使用本券請至萊爾富櫃檯直接出示本券掃碼兌換（請將螢幕亮度調到最大）。</li>
+                  <li>若無法成功掃碼，請出示本券兩段條碼序號，由門市人員輸入兌換。</li>
+                  <li>使用時須符合本券載明之品牌與規格。商品兌換後，無法提供退貨及換貨。</li>
+                  <li>本券不適用於免開立統一發票之代收/代售業務、菸酒商品及儲值(如：悠遊卡)。</li>
+                  <li>本券無法兌換現金或找零，請一次抵用完畢，不可重複使用。</li>
+                  <li>本券不得與其他行銷活動合併使用。</li>
+                  <li>本券不適用非開立萊爾富發票之特殊門市 (如：台鐵門市、部分學校、廠辦及商場門市)與離島門市。</li>
+                  <li>發行人：Edenred Taiwan 新加坡商宜睿智慧股份有限公司台灣分公司。</li>
+                  <li>本券為企業贈品專用，請於指定日期前兌換，不得零售或轉售。</li>
+                  <li>本券為不記名，任何人持本券皆可使用，請自行妥善保管，如遭他人盜用，本券不再補發。</li>
+                  <li>本券所兌換之商品或折抵消費之金額不予開立統一發票。</li>
+                  <li>本券有效與否，以發行人票券系統所記錄之狀態為憑。 如系統因網路連線有所遲延，依兌換商家系統端資訊為準。</li>
+                  <li>本券為有價證券，請勿擅自偽造、變造，以免觸犯刑責。</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      
       </div>
       
     </div>
 
+    <!-- confirm -->
+    <div class="confirm-wrap text-center" :class="{ 'active': doubleConfirm }">
+      <button type="button" class="close" @click="closeConfirm">&times;</button>
+      <strong class="d-block">確認兌換</strong>
+      <small class="d-block">使用320商周幣兌換嗎？</small>
+      <button type="button" class="button d-block" @click="couponAPI">確認</button>
+    </div>
+
     <!-- fixed bottom button -->
     <div class="fixed-button" :class="{ 'active': couponDeatilOpen }">
-      <div class="confirm text-center" :class="{ 'active': doubleConfirm }">
-        <button type="button" class="close" @click="closeConfirm">&times;</button>
-        <strong class="d-block">確認兌換</strong>
-        <small class="d-block">使用320商周幣兌換嗎？</small>
-      </div>
-      <button type="button" class="button d-block" @click="openCouponComfirm" v-if="!doubleConfirm">立即兌換</button>
-      <button type="button" class="button d-block" v-else>確認</button>
+      <button type="button" class="button d-block" @click="openCouponComfirm">立即兌換</button>
     </div>
 
   </section>
@@ -108,6 +132,10 @@
 
 
 <script>
+  // plugin waves
+  import waveCSS from '../../../node_modules/node-waves/dist/waves.css'
+  import Waves from 'node-waves'
+  
   export default {
     data() {
       return {
@@ -133,12 +161,19 @@
         this.confirm = false;
         this.doubleConfirm = false;
       },
+      // 使用者確定兌換後，要執行的動作
+      couponAPI() {},
+      // waves effect
+      waves() {
+        Waves.attach('.waves', ['waves-button', 'waves-float', 'waves-light']);
+        Waves.init();
+      },
       // collapse
       collapse() {
         // 寫入各 .content-box 的高，然後高度設成 0
         const contents = document.querySelectorAll('.collapse-group .contents-box');
         Array.prototype.forEach.call(contents, c => {
-          c.dataset.height = c.offsetHeight;
+          c.dataset.height = c.offsetHeight + 28;
           c.setAttribute('style', 'height: 0');
         });
 
@@ -166,13 +201,14 @@
     },
     mounted() {
       this.collapse();
+      this.waves();
     }
   };
 </script>
 
 
-
 <style scoped lang="less">
+
   // helper class
   .d-block {
     display: block;
@@ -181,15 +217,21 @@
     text-align: center;
   }
 
+  #coupon {
+    position: relative;
+  }
+
+
+  // 頂部banner，內容未確認，會再改
   .coupon_page_title {
-    padding-top: 30px;
+    padding-top: 120px;
     background: #dcdcdc;
   }
   
   .div_title {
     margin-bottom: 28px;
     padding-left: 16px;
-    border-left: 5px solid #cd0505;
+    border-left: 8px solid #cd0505;
     line-height: 40px;
     color: #424242;
     font-weight: bold;
@@ -205,11 +247,11 @@
     display: flex;
     flex-wrap: wrap;
     justify-content: space-between;
-    padding-right: 8px;
-    padding-left: 8px;
+    padding-right: 16px;
+    padding-left: 16px;
     a {
-      margin-bottom: 32px;
-      width: calc(50% - 16px);
+      margin-bottom: 56px;
+      width: calc(50% - 28px);
     }
     .img {
       width: 100%;
@@ -220,13 +262,17 @@
       background-size: cover;
     }
     .name {
-      font-size: 26px;
-      color: #000;
+      padding-top: 14px;
+      padding-bottom: 8px;
+      line-height: 40px;
+      font-size: 30px;
+      color: #424242;
     }
     .info {
       display: flex;
       align-items: center;
-      line-height: 24px;
+      line-height: 28px;
+      font-size: 28px;
       color: #adadad;
       span {
         text-decoration: line-through;
@@ -235,13 +281,13 @@
   }
 
   // coupon single detail
-  .coupon_deatil {
+  .coupon_detail {
     position: fixed;
     top: 0;
+    right: 0;
+    bottom: 0;
     left: 0;
-    width: 100%;
-    height: 100%;
-    padding-top: 42px;
+    padding-bottom: 50px;
     background: #000;
     opacity: 0;
     transform: translateX(100%);
@@ -250,40 +296,78 @@
       opacity: 1;
       transform: translateX(0);
     }
+    .top {
+      position: relative;
+      margin-bottom: 64px;
+      .close {
+        position: absolute;
+        right: 0;
+        appearance: none;
+        width: 66px;
+        height: 66px;
+        background: transparent;
+        font-size: 40px;
+        color: #FFF;
+      }
+    }
     .name {
-      margin-bottom: 22px;
+      margin-bottom: 64px;
+      font-size: 36px;
       color: #FFF;
+    }
+    figure {
+      display: block;
+      margin-bottom: 32px;
+      padding-right: 65px;
+      padding-left: 65px;
     }
     img {
       margin-right: auto;
       margin-left: auto;
-      margin-bottom: 14px;
+      max-width: 100%;
     }
     .info {
-      padding-top: 30px;
-      padding-bottom: 30px;
+      padding-top: 64px;
+      padding-bottom: 64px;
+      font-size: 28px;
       color: #FFF;
     }
 
     // overlay
-    &.overlay::before {
+    &::before {
       content: '';
       position: absolute;
       top: 0;
       left: 0;
-      z-index: 1;
+      z-index: -1;
       width: 100%;
       height: 100%;
       background: rgba(#000, .7);
+      opacity: 0;
+      transition: opacity .4s ease-in-out;
     }
+    &.overlay::before {
+      z-index: 2;
+      opacity: 1;
+    }
+  }
+
+  .coupon_detail_contents {
+    height: 100%;
+    overflow-y: auto;
   }
 
   // collapse
   .collapse-group {
+    padding-right: 36px;
+    padding-left: 36px;
+    padding-bottom: 36px;
     color: #FFF;
 
     .collapse {
       border-top: 1px solid #FFF;
+      line-height: 1.5;
+      font-size: 28px;
     }
 
     // collapse title
@@ -292,6 +376,7 @@
       display: flex;
       justify-content: space-between;
       align-items: center;
+      padding: 28px 0;
 
       // icons transform active
       &.active .icons {
@@ -300,7 +385,7 @@
             transform: rotate(180deg);
           }
           &:last-of-type {
-            transform: rotate(0) translateY(-2px);
+            transform: rotate(0) translateY(-4px);
           }
         }
       }
@@ -311,12 +396,12 @@
       position: absolute;
       right: 12px;
       span {
-        width: 12px;
-        height: 2px;
+        width: 28px;
+        height: 4px;
         background-color: #FFF;
         transition: transform .2s ease;
         &:last-of-type {
-          transform: rotate(90deg) translateX(-2px);
+          transform: rotate(90deg) translateX(-4px);
         }
       }
     }
@@ -324,28 +409,30 @@
     // collapse contents
     .contents-box {
       overflow: hidden;
+      text-align: justify;
       transition: height .3s ease;
     }
-    .collapse {
-      &:last-of-type .contents-box {
-        border-bottom: 1px solid #FFF;
-      }
+    .collapse:last-of-type {
+      border-bottom: 1px solid #FFF;
+    }
+
+    ul {
+      list-style: disc;
+      list-style-position: outside;
+      padding-left: 36px;
     }
   }
 
-  // fixed bottom button
-  .fixed-button {
+
+  .fixed-button, .confirm-wrap {
     position: fixed;
     bottom: 0;
     left: 0;
     right: 0;
-    z-index: 3;
-    &, .confirm {
-      opacity: 0;
-      transform: translateY(100%);
-      transition: transform .4s ease-in-out, opacity .4s ease-in-out;
-    }
-    &.active, .confirm.active {
+    opacity: 0;
+    transform: translateY(100%);
+    transition: transform .4s ease-in-out, opacity .4s ease-in-out;
+    &.active {
       opacity: 1;
       transform: translateY(0);
     }
@@ -353,19 +440,44 @@
       appearance: none;
       background: transparent;
     }
-    .confirm, .button {
-      position: relative;
-    }
-    .confirm {
-      z-index: 2;
-      background: #FFF;
-    }
     .button {
+      position: relative;
       z-index: 3;
+      padding-top: 16px;
+      padding-bottom: 16px;
       width: 100%;
       background: #CD0505;
+      font-size: 32px;
       color: #FFF;
     }
+  }
+
+  .confirm-wrap {
+    z-index: 3;
+    padding-top: 28px;
+    background: #FFF;
+    .close {
+      position: absolute;
+      top: 14px;
+      right: 28px;
+      margin-left: auto;
+      font-size: 28px;
+      color: #aaa;
+    }
+    strong {
+      padding-top: 14px;
+      padding-bottom: 28px;
+      font-size: 36px;
+    }
+    small {
+      padding-bottom: 28px;
+      font-size: 28px;
+    }
+  }
+
+  // fixed bottom button
+  .fixed-button {
+    z-index: 2;
   }
 
   // B coin
@@ -374,15 +486,15 @@
     justify-content: center;
     align-items: center;
     margin-right: 8px;
-    font-size: 14px;
+    font-size: 28px;
     color: #cd0505;
     &::before {
       content: '';
       display: inline-block;
       margin-top: -2px;
       margin-right: 8px;
-      width: 24px;
-      height: 24px;
+      width: 36px;
+      height: 36px;
       background: url("../../assets/img/bCoin@2x.png") no-repeat center/contain;
     }
   }
